e97f0df24d894d885e445ea5c9cfa352bf723ce4
Lucas-M01 <lucas.montenegro.n@outlook.com>
2022-08-19 18:11:29 -0300

    Componentes Sidebar e Avatar finalizado

--- modulo1/fundamentosReacJS/01-fundamentos-react/src/App.jsx
+++ modulo1/fundamentosReacJS/01-fundamentos-react/src/App.jsx
@@ -1,11 +1,21 @@
 import { Header } from './components/Header'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
 
+import styles from './App.module.css'
 import './global.css'
 
 export function App() {
   return (
     <div className="App">
       <Header/>
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+        </main>
+      </div>
     </div>
   )
 }
