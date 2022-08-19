688fd14940ae4c6dcdc331156e703c4b349d0496
Lucas-M01 <lucas.montenegro.n@outlook.com>
2022-08-19 18:40:17 -0300

    Componente Post finalizado

--- modulo1/fundamentosReacJS/01-fundamentos-react/src/components/Post.jsx
+++ modulo1/fundamentosReacJS/01-fundamentos-react/src/components/Post.jsx
@@ -1,5 +1,40 @@
+import { Avatar } from './Avatar'
+import styles from './Post.module.css'
+
 export function Post() {
     return (
-        <p></p>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar src='https://github.com/Lucas-M01.png' />
+                    <div className={styles.authorInfo}>
+                        <strong>Lucas Montenegro</strong>
+                        <span>Estudante</span>
+                    </div>
+                </div>
+
+                <time title='19 de Agosto às 06:22h' dateTime='2022-08-19 06:22:41' >Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="">jane.design/doctorcare</a></p>
+                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea />
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                
+            </div>
+            
+        </article>
     )
 }
