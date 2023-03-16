efb370b09df66ea68808324977a221db3a8f57fa
Lucas-M01 <lucas.montenegro.n@outlook.com>
2023-03-16 17:44:54 -0300

    Foi criado alguns componentes

--- Projeto5/05-design-system/packages/react/src/components/Avatar/index.tsx
+++ Projeto5/05-design-system/packages/react/src/components/Avatar/index.tsx
@@ -2,9 +2,7 @@ import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
 import { User } from 'phosphor-react'
 import { ComponentProps } from 'react'
 
-export interface AvatarProps extends ComponentProps<typeof AvatarImage> {
-
-}
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
 
 export function Avatar(props: AvatarProps) {
   return (
